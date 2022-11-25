369a7114a5c446a4c799372b81e6f0f84c92b560
michael <misfitmade2@gmail.com>
2022-11-25 15:39:46 -0700

    More updates organized + more deemed junk

--- FP567_Lib.py
+++ FP567_Lib.py
@@ -77,5 +77,99 @@ def check_if_init_is_game_updates(file_name: str):
     return num_toks == 3 and toks[0]=="#" and toks[1]=="Game" and toks[2]=="updates"
 
 
+def get_updates_from_certain_misc_updates_pattern(files_with_pattern) -> None:
+    '''
+    Found another common pattern amongst some files with some extractable updates that are different than
+    those that we already got from above notebook cells.
+    The below code parses this pattern, saving the updates.
+    First, to more easily keep track of what I have procd and not, move those that I process over to
+    a "has_been_processed" sub folder.
+    '''
+    for file_name in files_with_pattern:
+        path_strt = os.path.join(PATH_TO_RAW_MISC_UPDATES, file_name)
+        if os.path.exists(path_strt):
+            lines = open(path_strt).readlines()
+            i = 0
+            while i < len(lines):
+                toks = lines[i].split()
+                if len(toks) == 3 and toks[0] == "Date" and toks[2] == "Changes":
+                    i = i + 1
+                    toks = lines[i].split()
+                    while (toks[0] != "##"):
+                        # get the date line as a folder path
+                        d = toks[0]
+                        path_to_day_folder = os.path.join(
+                            PATH_TO_PROCESSED_UPDATES_BY_YEAR,
+                            toks[2][2:4],
+                            word_month_to_number_month(toks[1]),
+                            d)
+                        os.makedirs(path_to_day_folder, exist_ok=True)
+                        # gather the lines below
+                        per_day_update_ctr = len(os.listdir(path_to_day_folder))
+                        i = i + 1
+                        line_i = lines[i]
+                        toks = line_i.split()
+                        while len(toks) > 0:
+                            print(path_to_day_folder)
+                            with open(os.path.join(path_to_day_folder, f"{d}_{per_day_update_ctr}.txt"), 'w') as f:
+                                f.write(line_i)
+                            per_day_update_ctr = per_day_update_ctr + 1
+                            i = i + 1
+                            line_i = lines[i]
+                            toks = line_i.split()
+
+                        i = i + 1
+                        toks = lines[i].split()
+
+                    shutil.move(path_strt, os.path.join(PATH_TO_RAW_MISC_UPDATES_BEEN_PROCD, file_name))
+
+                i = i + 1
+
+
+def get_updates_from_other_certain_misc_updates_pattern(files_with_pattern) -> None:
+    '''
+    Another found pattern of updates to extract:
+    ## Year
+
+    day month
+    update
+    '''
+    for file_name in files_with_pattern:
+        path_strt = os.path.join(PATH_TO_RAW_MISC_UPDATES, file_name)
+        print(path_strt)
+        if os.path.exists(path_strt):
+            print("YAY")
+            lines = open(path_strt).readlines()
+            i = 0
+            while i < len(lines):
+                toks = lines[i].split()
+                if len(toks) == 2:
+                    maybe_yr = toks[1]
+                    if toks[0] == "##" and maybe_yr in RUNESCAPE_YEARS:
+                        i = i + 1
+                        toks = lines[i].split()
+                        while len(toks) > 0:
+                            d = toks[0]
+                            path_to_day_folder = os.path.join(
+                                PATH_TO_PROCESSED_UPDATES_BY_YEAR,
+                                maybe_yr[2:4],
+                                word_month_to_number_month(toks[1]),
+                                d)
+                            os.makedirs(path_to_day_folder, exist_ok=True)
+                            print(path_to_day_folder)
+                            per_day_update_ctr = len(os.listdir(path_to_day_folder))
+                            i = i + 1
+                            with open(os.path.join(path_to_day_folder, f"{d}_{per_day_update_ctr}.txt"), 'w') as f:
+                                f.write(lines[i])
+
+                            i = i + 1
+                            toks = lines[i].split()
+
+                i = i + 1
+
+            shutil.move(path_strt, os.path.join(PATH_TO_RAW_MISC_UPDATES_BEEN_PROCD, file_name))
+
+
+
 def word_month_to_number_month(month: str) -> str:
     return WORD_MONTH_TO_NUMBER_MAP[month.lower()]
